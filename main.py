951589a0b21eb2d6896e55ca8a6296841442cf90
Piyush <piyushpalan0877@gmail.com>
2026-06-14 04:13:45 +0530

    version-2

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import random
+
 print("Welcome to Guess The Number")
 
-secret_number = 7
+secret_number = random.randint(1, 10)
 
-guess = int(input("Enter a number: "))
+guess = int(input("Enter a number between 1 and 10: "))
 
 if guess == secret_number:
     print("Correct!")
+
+elif guess < secret_number:
+    print("Too low!")
+
 else:
-    print("Wrong!")
+    print("Too high!")
